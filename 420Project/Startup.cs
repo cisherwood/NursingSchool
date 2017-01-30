17c1662a7420479291ff10f14ac81ced7125effc
Mugic <Mugic@LAPTOP-TJTQR672>
2017-01-30 16:37:20 -0500

    Add project files.

---
+++ 420Project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_420Project.Startup))]
+namespace _420Project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
