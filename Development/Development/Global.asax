ce00d6f8dc76d6040344703d70a6ed389c2ab734
Admin <cisherwood@p-ca.com>
2017-02-01 19:16:01 -0500

    2-1-2017developinit

---
+++ Development/Development/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Development.MvcApplication" Language="C#" %>
---
+++ Development/Development/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Development.Startup))]
+namespace Development
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
