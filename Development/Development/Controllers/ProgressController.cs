fcf1dd0fbb1ce140afc3c896b361c4e5378b9a4b
Chris <Chris@DESKTOP-SUBQ115>
2017-02-06 16:46:27 -0800

    2-6whatever

---
+++ Development/Development/Controllers/ProgressController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Development.Controllers
+{
+    public class ProgressController : Controller
+    {
+        // GET: Progress
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
