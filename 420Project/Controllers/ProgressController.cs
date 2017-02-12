e7827e768a930ac292569611dff5c90a4860b65e
Admin <cisherwood@p-ca.com>
2017-02-11 20:08:29 -0500

    2-11nearend

--- 420Project/Controllers/ProgressController.cs
+++ 420Project/Controllers/ProgressController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using _420Project.Models;
+using _420Project.ViewModels;
 
 namespace _420Project.Controllers
 {
@@ -39,6 +40,9 @@ namespace _420Project.Controllers
             // Get current semester ID from session variable
             int currentSemesterId = Convert.ToInt32(Session["CurrentSemesterId"]);
 
+            // Set selected semester ID
+            Session["ProgresStudentCurrentSemester"] = currentSemesterId;
+
             ViewBag.currentSemesterId = currentSemesterId;
 
             // Get enrollment data about selected student
@@ -69,8 +73,8 @@ namespace _420Project.Controllers
             // Get selected student id
             int currentStudentId = Convert.ToInt32(Session["ProgressStudentCurrentStudent"]);
 
-            // Get current semester ID from session variable
-            int currentSemesterId = Convert.ToInt32(Session["CurrentSemesterId"]);
+            // Set selected semester ID
+            Session["ProgresStudentCurrentSemester"] = sem;
 
             // Get enrollment data about selected student
             List<Enrollment> enrollments = new List<Enrollment>(from en in db.Enrollment
@@ -79,6 +83,36 @@ namespace _420Project.Controllers
                                                                 select en);
 
             return View(enrollments);
+        }
+
+        //AJAX
+        // Get class
+        public ActionResult StudentGetAddCourses(int? sem)
+        {
+            // List of classes
+            List<Course> courses = db.Course.ToList();
+
+            int currentStudent = Convert.ToInt32(Session["ProgressStudentCurrentStudent"]);
+            int currentSemester = Convert.ToInt32(Session["ProgresStudentCurrentSemester"]);
+
+            List<ProgressStudentAddCourseViewModel> vm = new List<ProgressStudentAddCourseViewModel>();
+
+            foreach (Course c in courses)
+            {
+                ProgressStudentAddCourseViewModel course = new ProgressStudentAddCourseViewModel();
+                course.CourseId = c.CourseId;
+                course.Number = c.Number;
+                course.Title = c.Title;
+                course.Description = c.Description;
+                course.Department = c.Department;
+                course.IsChecked = db.Enrollment.Any(x => x.CourseId == c.CourseId && x.SemesterId == currentSemester && x.StudentId == currentStudent);
+                course.HasTaken = db.Enrollment.Any(x => x.CourseId == c.CourseId && x.StudentId == currentStudent);
+
+                vm.Add(course);
+
+            }
+
+            return View(vm);
         }
 
         // AJAX
@@ -93,14 +127,6 @@ namespace _420Project.Controllers
             return View(courses);
         }
 
-        //AJAX
-        // Get class
-        public ActionResult StudentGetAddCourses(int? sem)
-        {
-            // List of classes
-            List<Course> courses = db.Course.ToList();
 
-            return View(courses);
-        }
     }
 }
---
+++ 420Project/ViewModels/ProgressStudentAddCourseViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using _420Project.Models;
+
+namespace _420Project.ViewModels
+{
+    public class ProgressStudentAddCourseViewModel
+    {
+        public int CourseId { get; set; }
+        public string Number { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string CourseString { get { return this.Department.Name + " " + this.Number; } }
+        public bool IsChecked { get; set; }
+        public bool HasTaken { get; set; }
+
+        public virtual Department Department { get; set; }
+
+
+    }
+}
