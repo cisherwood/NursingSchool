6173e5a75b946a6f2b0ad85209e72b7f32e149b3
ablen <ablen@DESKTOP-RVRCHC4>
2017-02-22 16:23:55 -0500

    Changes

--- 420Project/Models/Campus.cs
+++ 420Project/Models/Campus.cs
@@ -2,23 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace _420Project.Models
 {
     public class Campus
     {
         public int CampusID { get; set; }
-
+        
+        [StringLength(50)]
         public string CampusName { get; set; }
 
+        [StringLength(50)]
         public string Address { get; set; }
 
+        [StringLength(50)]
         public string City { get; set; }
 
+        [StringLength(20)]
         public string State { get; set; }
 
+        [StringLength(10)]
         public string ZipCode { get; set; }
 
+        [StringLength(15)]
         public string PhoneNumber { get; set; }
         
     }
--- 420Project/Models/Student.cs
+++ 420Project/Models/Student.cs
@@ -2,24 +2,36 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace _420Project.Models
 {
     public class Student
     {
         public int StudentId { get; set; }
+
+        [StringLength(50, MinimumLength = 1)]
         public string FirstName { get; set; }
+
+        [StringLength(50, MinimumLength = 1)]
         public string LastName { get; set; }
 
+        [StringLength(50, MinimumLength = 1)]
         public string MiddleName { get; set; }
 
+        [StringLength(30, MinimumLength = 1)]
         public string Email { get; set; }
 
+        [StringLength(15, MinimumLength = 1)]
         public string PhoneNumber { get; set; }
+
+        [StringLength(50, MinimumLength = 1)]
         public string Address { get; set; }
         public int AdvisorId { get; set; }
         public int ProgramId { get; set; }
         public bool HasGraduated { get; set; }
+
+        [StringLength(5, MinimumLength = 1)]
         public string Standing { get; set; }
         public string Year { get; set; }
         public DateTime DOB { get; set; }
