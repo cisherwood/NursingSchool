a6afeab797d8a2e53d9d72ee156251450d8a269c
Mugic <Mugic@LAPTOP-TJTQR672>
2017-02-22 15:05:23 -0500

    Compliance and StudentCompliance Models Added

---
+++ 420Project/Models/Compliance.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _420Project.Models
+{
+    public class Compliance
+    {
+        public int ComplianceId;
+        public string Name { get; set; }
+
+        
+
+    }
+}
