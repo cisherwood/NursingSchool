7df39eeb8a4dc5b10cc89319dce746fb0c638473
ablen <ablen@DESKTOP-RVRCHC4>
2017-02-22 15:10:40 -0500

    2/22/17

---
+++ 420Project/Models/To_Do.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _420Project.Models
+{
+    public class To_Do
+    {
+        public int ToDoID { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime CreateDate { get; set; }
+
+        public DateTime DueDate { get; set; }
+
+        public int GroupID { get; set; }
+    }
+}
