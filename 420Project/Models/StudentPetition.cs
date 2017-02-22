9b8d7a29080ec7b39c13153f178760d32b5d4b99
Brooke Gorbandt <Brookegorbandt@gmail.com>
2017-02-22 15:05:52 -0500

    2/22 Brooke - Petition Models

---
+++ 420Project/Models/StudentPetition.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _420Project.Models
+{
+    public class StudentPetition
+    {
+        public int StudentPetitionID { get; set; }
+        public string Status { get; set; }
+        public DateTime SubmitDate { get; set; }
+
+        public int StudentID { get; set; }
+        public int PetitionID { get; set; }
+    }
+}
