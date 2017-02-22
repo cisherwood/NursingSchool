60b90049f9e98dc164ec67dcd9670770a193703b
Mugic <Mugic@LAPTOP-TJTQR672>
2017-02-22 17:22:03 -0500

    GetGPA and GetGradePoint (Helper Method) made in SystemController

--- 420Project/Controllers/SystemController.cs
+++ 420Project/Controllers/SystemController.cs
@@ -11,59 +11,65 @@ namespace _420Project.Controllers
     {
         private ApplicationDbContext db = new ApplicationDbContext();
 
-        //public double GetGPA(int id)
-        //{
-        //    double totalHours, achievedPoints = 0;
+        //Method that will be take a student's Id, retrieve their enrollments, and then calculate their GPA.
+        //Pre-Condition: Provide a valid Id number.
+        //Post-Conition: The student's GPA will be returned.
+        public double GetGPA(int id)
+        {
+            double totalHours = 0;
+            double achievedPoints = 0;
 
-        //    var enrollments = from c in db.Enrollment
-        //                      where c.Student.StudentId == id
-        //                      select c;
+            var enrollments = from c in db.Enrollment
+                              where c.Student.StudentId == id
+                              select c;
 
-        //    foreach (Enrollment enrollment in enrollments)
-        //        totalHours += enrollment.Course.ClassHours;
+            foreach (Enrollment enrollment in enrollments)
+                totalHours += enrollment.Course.ClassHours;
 
-        //    foreach (Enrollment enrollment in enrollments)
-        //        achievedPoints += GetGradePoints(enrollment.Course.ClassHours, enrollment.Grade);
+            foreach (Enrollment enrollment in enrollments)
+                achievedPoints += GetGradePoints(enrollment.Course.ClassHours, enrollment.Grade);
 
-        //}
+            return achievedPoints / totalHours;
+        }
+        //Helper method that will determine the grade points achieved at a certain grade.
+        //Pre-Condition: Class Hours and Grade achieved are passed.
+        //Post-Condition: GradePoints earned are returned.
+        private double GetGradePoints(int hours, string grade)
+        {
+            switch (grade)
+            {
+                case "A+":
+                    return hours * 4;
+                case "A":
+                    return hours * 4;
+                case "A-":
+                    return hours * 3.7;
+                case "B+":
+                    return hours * 3.3;
+                case "B":
+                    return hours * 3;
+                case "B-":
+                    return hours * 2.7;
+                case "C+":
+                    return hours * 2.3;
+                case "C":
+                    return hours * 2.0;
+                case "C-":
+                    return hours * 1.7;
+                case "D+":
+                    return hours * 1.3;
+                case "D":
+                    return hours * 1;
+                case "D-":
+                    return hours * .7;
+                case "F":
+                    return hours * 0;
+                default:
+                    return hours * 0;
+            }
 
-        //private double GetGradePoints(int hours, string grade)
-        //{
-        //    switch (grade)
-        //    {
-        //        case "A+":
-        //            return hours * 4;
-        //        case "A":
-        //            return hours * 4;
-        //        case "A-":
-        //            return hours * 3.7;
-        //        case "B+":
-        //            return hours * 3.3;
-        //        case "B":
-        //            return hours * 3;
-        //        case "B-":
-        //            return hours * 2.7;
-        //        case "C+":
-        //            return hours * 2.3;
-        //        case "C":
-        //            return hours * 2.0;
-        //        case "C-":
-        //            return hours * 1.7;
-        //        case "D+":
-        //            return hours * 1.3;
-        //        case "D":
-        //            return hours * 1;
-        //        case "D-":
-        //            return hours * .7;
-        //        case "F":
-        //            return hours * 0;
-        //        case default:
-        //            return hours * 0;
 
-        //    }
-
-
-        //}
+        }
 
 
 
