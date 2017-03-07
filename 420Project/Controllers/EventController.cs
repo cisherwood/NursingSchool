adcbd08d23b8168a406fccf802b224f85cb58253
Chris <Chris@DESKTOP-SUBQ115>
2017-03-06 17:08:12 -0800

    class example

---
+++ 420Project/Controllers/EventController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _420Project.Controllers
+{
+    public class EventController : Controller
+    {
+        // GET: Event
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        // GET: Event
+        public ActionResult Detail(int id)
+        {
+
+            return View();
+        }
+    }
+}
