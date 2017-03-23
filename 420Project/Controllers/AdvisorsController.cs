429902e912b53fa46a3334d797cda1ea7a181f84
Joey <Joey@DESKTOP-jkunks317@aol.com>
2017-03-23 18:48:05 -0400

    Final Joey changes. 3/23/17

---
+++ 420Project/Controllers/AdvisorsController.cs
@@ -0,0 +1,130 @@
+﻿//Joey Kunkel
+//3/23/17
+//Advisor Controller
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using _420Project.Models;
+
+namespace _420Project.Controllers
+{
+    public class AdvisorsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Advisors
+        public ActionResult Index()
+        {
+            return View(db.Advisor.ToList());
+        }
+
+        // GET: Advisors/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Advisor advisor = db.Advisor.Find(id);
+            if (advisor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(advisor);
+        }
+
+        // GET: Advisors/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Advisors/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "AdvisorId,FirstName,LastName,Title,TitleDescription,Office,UserId,Email,ContactNumber")] Advisor advisor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Advisor.Add(advisor);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(advisor);
+        }
+
+        // GET: Advisors/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Advisor advisor = db.Advisor.Find(id);
+            if (advisor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(advisor);
+        }
+
+        // POST: Advisors/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "AdvisorId,FirstName,LastName,Title,TitleDescription,Office,UserId,Email,ContactNumber")] Advisor advisor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(advisor).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(advisor);
+        }
+
+        // GET: Advisors/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Advisor advisor = db.Advisor.Find(id);
+            if (advisor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(advisor);
+        }
+
+        // POST: Advisors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Advisor advisor = db.Advisor.Find(id);
+            db.Advisor.Remove(advisor);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
