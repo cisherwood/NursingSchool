1f4096667b48ee5159f621f04e866fdbb21f3d5d
Brent <Brent@DESKTOP-9IBK3BA>
2017-03-26 17:11:22 -0400

    Brent 3/26 model changes

--- 420Project/Models/ProgramCourse.cs
+++ 420Project/Models/ProgramCourse.cs
@@ -1,5 +1,11 @@
-﻿using System;
+﻿//Brentan Hudson
+//2/24/17
+//bridge between LD BSN, and courses required for that program
+
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -9,13 +15,24 @@ namespace _420Project.Models
     public class ProgramCourse
     {
         public int ProgramCourseId { get; set; }
+
+        [ForeignKey("Program")]
+        [Display(name = "Program")]
+        [Required]
         public int ProgramId { get; set; }
+
+        [Display(Name = "Course ID")]
+        [Required]
         public int CourseId { get; set; }
-        public int SemesterId { get; set; }
+        [Display(Name = "Semester")]
+        [Required]
+        public int SemesterNumber { get; set; }
 
         public virtual Course Course { get; set; }
         public virtual Program Program { get; set; }
+        //326
 
+        
 
     }
 }
