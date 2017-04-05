18d1d8d28115ea6648fe0e94a52853df0a011bcf
Admin <cisherwood@p-ca.com>
2017-04-05 09:03:05 -0400

    4/4/2017

---
+++ 420Project/Controllers/GroupStuffController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _420Project.Controllers
+{
+    public class GroupStuffController : Controller
+    {
+        // GET: GroupStuff
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult _GroupFilters()
+        {
+
+
+
+            return View();
+        }
+    }
+}
