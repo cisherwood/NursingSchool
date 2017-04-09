c79f7861ca722b3710b5b7f730de994706d71309
Admin <cisherwood@p-ca.com>
2017-04-09 19:25:51 -0400

    enrollment frontend start

--- 420Project/Controllers/EnrollmentController.cs
+++ 420Project/Controllers/EnrollmentController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _420Project.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,15 +9,31 @@ namespace _420Project.Controllers
 {
     public class EnrollmentController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
         // GET: Enrollment
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult _Student()
+        public ActionResult _Student(List<int> studentIDList)
         {
-            return View();
+            List<Student> studentList = new List<Student>();
+
+            if (studentIDList != null)
+            {
+                foreach (int id in studentIDList)
+                {
+                    studentList.Add((db.Student.Where(x => x.StudentId == id)).SingleOrDefault());
+                }
+            }
+            else
+            {
+                studentList = db.Student.Where(x => x.StudentId != 0).ToList(); //Change to only enrolled students.
+            }
+
+            return View(studentList);
         }
 
         public ActionResult _Course()
